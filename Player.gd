ca41eaf688a951e55c3b90d9e5c681dd2b9f0023
Number6406 <6406.number@gmail.com>
2020-04-18 13:33:49 +0200

    Simple running and physics implemented

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+const gravity = 98.1
+export var speed = 500
+export var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	
+	if is_on_floor() :
+		velocity = direction * speed
+		velocity.y = gravity
+		if Input.is_action_just_pressed("jump"):
+			velocity += Vector2.UP * 2000
+	else:
+		velocity += gravity * Vector2.DOWN
+	print(velocity)
+	move_and_slide(velocity, Vector2.UP)
+	pass
